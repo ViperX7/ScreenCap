0dc80d9b8cce64206f88997e808bcb49f058f21a
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2019-07-27 18:02:42 +0530

    Initial Implementation : Takes cont.. screenshots This version just takes continous screenshots and saves them to a directory which can later be converted to gif manually

---
+++ gifcap.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+OUTPUT_DIRECTORY=$1
+FRAMERATE=$2
+STARTUP_DELAY=$3
+
+SCREENSHOT_DELAY=$(expr 100 \/ $FRAMERATE)
+
+wait=$STARTUP_DELAY
+
+delay(){
+    if [ $wait != 0 ];then
+        echo  Starting Recording in $wait
+        wait=$(expr $wait - 1)
+        sleep 1
+        delay
+
+    else
+        sleep 1
+    fi
+
+}
+num=1
+
+gifcap()
+{
+    sleep $FRAMERATE
+    file=$OUTPUT_DIRECTORY'/'$num'.jpg'
+    scrot $file -q 100
+    num=$(expr $num + 1)
+    gifcap
+}
+
+delay
+gifcap

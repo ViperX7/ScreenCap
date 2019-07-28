8977ad99eb938f298b15dbde66c5d0929947665c
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2019-07-28 21:21:40 +0530

    BugFixes And Verbosity Improvements     Format changed from jpg to png     Automatic Handelling of folder creation

--- gifcap.sh
+++ gifcap.sh
@@ -22,10 +22,26 @@ delay(){
 }
 num=1
 
+if [ -e $OUTPUT_DIRECTORY ]
+then
+    echo Writing Files to $OUTPUT_DIRECTORY
+else
+    echo -n "$OUTPUT_DIRECTORY not found trying to create one"
+    mkdir -p $OUTPUT_DIRECTORY
+    if [ -e $OUTPUT_DIRECTORY ]
+    then
+        echo $(tput setaf 2) Sucessed $(tput sgr0)
+    else
+        echo $(tput setaf 1) Failed $(tput sgr0)
+        echo exitting
+        exit
+    fi
+fi
+
 gifcap()
 {
     sleep $FRAMERATE
-    file=$OUTPUT_DIRECTORY'/'$(python3 rename.py $num'.jpg')
+    file=$OUTPUT_DIRECTORY'/'$(python3 rename.py $num'.png')
     scrot $file -q 100
     num=$(expr $num + 1)
     gifcap

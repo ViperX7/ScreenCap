16a5dd62f839401018db56105acbcebd60a7d90f
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2019-09-10 23:53:44 +0530

    Lots of new features and improvements added command line arguments support added default parameteres added help message added banner header added proper exit code support

--- gifcap.sh
+++ gifcap.sh
@@ -1,17 +1,82 @@
 #!/bin/sh
 
-OUTPUT_DIRECTORY=$1
-FRAMERATE=$2
-STARTUP_DELAY=$3
 
-SCREENSHOT_DELAY=$(expr 100 \/ $FRAMERATE)
+# Strings
 
-wait=$STARTUP_DELAY
+help='usage: imgtool [-h] [-o output] [-f FRAMERATE] [-d DELAY]
+\n
+\noptional arguments:
+\n
+\n\t  -o PATH, --out path\t\t\t            Output Directory where screenshots will be saved
+\n\t  -f FRAMERATE, --framerate FRAMERATE\t            Number of seconds to wait between two frames
+\n\t  -d DELAY , --dekay DELAY\t\t           Delay before recording starts
+\n\t  -h, --help\t\t\t\t           Show this help message and exit'
+
+title="GIFCap"
+
+banner()
+{
+BANNER="####################################\n\t\t$title\n####################################"
+}
+
+# Exit Function
+    quit()
+    {
+        return $@
+        exit
+    }
+
+
+# DEFAULT PARAMETERES
+
+
+OUTPUT_DIRECTORY=$(date +%F-%H_%M_%s)
+FRAMERATE=0.1
+DELAY=5
+
+
+# Command Line Argument Parsing
+
+
+while [ $# -gt 0 ]
+do
+    key=$1
+    case $key in
+        -o|--out)
+            OUTPUT_DIRECTORY=$2
+            shift
+            shift
+            ;;
+        -f|--framerate)
+            FRAMERATE=$2
+            shift
+            shift
+            ;;
+        -d|--delay)
+            DELAY=$2
+            shift
+            shift
+            ;;
+       -h|--help)
+            echo -e $help
+            shift
+            ;;
+        *)    # unknown option
+            echo Unknown parameter $key
+            echo -e $help
+            quit 2
+            shift # past argument
+            ;;
+    esac
+done
+
+
+
 
 delay(){
-    if [ $wait != 0 ];then
-        echo  -ne "\r$(tput setaf 1)Recording in "$(tput setaf 3)"$wait"
-        wait=$(expr $wait - 1)
+    if [ $DELAY != 0 ];then
+        echo  -ne "\r$(tput setaf 1)Recording in "$(tput setaf 3)"$DELAY"
+        DELAY=$(expr $DELAY - 1)
         sleep 1
         delay
 
@@ -23,21 +88,26 @@ delay(){
 }
 num=1
 
-if [ -e $OUTPUT_DIRECTORY ]
-then
-    echo Writing Files to $OUTPUT_DIRECTORY
-else
-    echo -n "$OUTPUT_DIRECTORY not found trying to create one"
-    mkdir -p $OUTPUT_DIRECTORY
-    if [ -e $OUTPUT_DIRECTORY ]
+dir_chk()
+{
+    if [ -e $1 ]
     then
-        echo $(tput setaf 2) Sucessed $(tput sgr0)
+        echo Writing Files to $OUTPUT_DIRECTORY
     else
-        echo $(tput setaf 1) Failed $(tput sgr0)
-        echo exitting
-        exit
+        echo -n "$1 not found trying to creating $1"
+        mkdir -p $1
+        if [ -e $1 ]
+        then
+            echo $(tput setaf 2) Sucessed $(tput sgr0)
+        else
+            echo $(tput setaf 1) Failed $(tput sgr0)
+            echo exitting
+            exit
+        fi
     fi
-fi
+}
+
+
 
 gifcap()
 {
@@ -48,5 +118,18 @@ gifcap()
     gifcap
 }
 
-delay
-gifcap
+
+
+execute()
+{
+    banner
+    echo -e $BANNER
+    dir_chk $OUTPUT_DIRECTORY
+    delay
+    gifcap
+
+
+}
+
+
+execute

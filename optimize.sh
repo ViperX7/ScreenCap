d8bdfb9d5357b03877fbc477eec62e3d26349029
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2019-09-11 01:12:43 +0530

    Auto redundant frame removal and renumbering diff output supressed Now frames are auto renumbered after removing redundant frames

--- optimize.sh
+++ optimize.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 INPUT=$1
-OUTPUT_FILE=$2
-IN_FPS=$3
-OUT_FPS=$4
 
 for x in $(ls $INPUT) 
 do
-    diff $INPUT/$(printf %04d $(expr  $(echo $x |head -c -5) - 1)).png $INPUT/$x
+    diff $INPUT/$(printf %04d $(expr  $(echo $x |head -c -5) - 1)).png $INPUT/$x 1>/dev/null
     if [ $? -eq 0 ];then
-        rm $INPUT/$(printf %04d $(expr  $(echo $x |head -c -5) - 1 )).png
+        echo Removing $INPUT/$(printf %04d $(expr  $(echo $x |head -c -5) - 1 )).png
+        rm $INPUT/$(printf %04d $(expr  $(echo $x |head -c -5) - 1 )).png 2>/dev/null
     fi
 done
+
+
+echo Realigning and preparing for compilation
+if [ $2 -eq '']
+then
+    i=1
+    for x in $(ls $INPUT/)
+    do
+    mv $INPUT/$x $INPUT/$(printf %04d $i).png
+    i=$(expr $i + 1)
+    done
+fi
